7cf3f11eee6dabc9664be71714b519444e7a6e00
Jatin2753 <140484456+Jatin2753@users.noreply.github.com>
2023-08-23 22:47:47 +0530

    Update greaternum.c
    adding smaller and middle

--- greaternum.c
+++ greaternum.c
@@ -11,15 +11,44 @@ int main()
     
     if(fnum>Snum && fnum>Tnum)
     {
-         printf("%d is greater",fnum);
+         printf("\n%d is greater",fnum);
     }
     else  if(fnum<Snum && Snum>Tnum)
     {
-         printf("%d is greater",Snum);
+         printf("\n%d is greater",Snum);
     }
     else
     {
-        printf("%d is greater",Tnum);
+        printf("\n%d is greater",Tnum);
+    }
+    
+    
+    
+    if(fnum<Snum && fnum<Tnum)
+    {
+         printf("\n%d is smaller",fnum);
+    }
+    else  if(fnum>Snum && Snum<Tnum)
+    {
+         printf("\n%d is smaller",Snum);
+    }
+    else
+    {
+        printf("\n%d is smaller",Tnum);
+    }
+    
+
+    if(fnum>Snum && fnum<Tnum || fnum<Snum && fnum>Tnum )
+    {
+         printf("\n%d is middle number",fnum);
+    }
+    else  if(fnum>Snum && Snum<Tnum || fnum<Snum && Snum<Tnum)
+    {
+         printf("\n%d is middle number",Snum);
+    }
+    else
+    {
+        printf("\n%d is middle number",Tnum);
     }
     
     
