24f47274e505673cd3461a905428ad30f5d59e7d
Jatin2753 <140484456+Jatin2753@users.noreply.github.com>
2023-09-14 11:25:19 +0530

    size of
    size of

---
+++ sizeof.c
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+   printf("char occupies %d byte\n",sizeof(char));
+   printf("\nint occupies %d byte\n",sizeof(int));
+   printf("\nfloat occupies %d byte\n",sizeof(float));
+    printf("\ndouble occupies %d byte\n",sizeof(double));
+ 
+    return 0;
+}
